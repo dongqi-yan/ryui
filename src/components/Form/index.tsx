3ba68b3ad2403a52e563b49db08066388969e6ab
russellyan <xhy8293@163.com>
2021-07-04 13:37:54 +0800

    add menu

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface PropsType {
+
+}
+
+class Menu extends React.Component<PropsType> {
+  constructor(props: PropsType) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
+
+
+
+
